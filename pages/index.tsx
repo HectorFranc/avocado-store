475d2e8926d02416a9099128e5d647e470ea4b08
Hector Franco T <tor6.franco6@hotmail.com>
2020-08-14 21:44:45 -0500

    Fetch API try

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import Navbar from '../components/Navbar/Navbar'
 
 const HomePage = () => {
+  const [productList, setProductList] = useState<TProduct[]>([])
+
+  useEffect(() => {
+    window
+      .fetch('/api/avo')
+      .then((data) => data.json())
+      .then(({ data }) => setProductList(data))
+  }, [])
+
   return (
     <div>
       <Navbar />
       <div>Next.js!</div>
+
+      {productList.map((product) => (
+        <p>{product.name}</p>
+      ))}
     </div>
   )
 }
