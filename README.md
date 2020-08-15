a31de99075b8e9b981fc23caceb86ed5ba57829e
Hector Franco T <tor6.franco6@hotmail.com>
2020-08-14 21:12:54 -0500

    Project structure

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # avocado-store
+
 Tienda ficticia de aguacates creada con NextJs y TypeScript.
+
+## ¿Quieres probarlo?
+
+Instala dependencias:
+
+```sh
+yarn
+```
+
+Inicia el proyecto:
+
+```sh
+yarn dev
+```
+
+El sitio estará disponible en http://localhost:3000.
---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+
+export default function Navbar() {
+  return (
+    <nav>
+      <menu>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </menu>
+    </nav>
+  )
+}
